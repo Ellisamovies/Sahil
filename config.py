5f3f9d661c89d971f92242556f5d4f8c07b3b92c
Ellisamovies <121487082+Ellisamovies@users.noreply.github.com>
2024-09-04 23:19:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002064170254"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002188290902"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
